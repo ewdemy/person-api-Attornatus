baaeb21f61c786df1021506dc0076af56bfe5e32
ewdemy <ewdemy@hotmail.com>
2023-01-13 11:34:42 -0300

    Datasource

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into ENDERECO(logradouro, cep, numero, cidade, principal) values ('Avenida Afonso Pena', '30130005', '7', 'Belo Horizonte', true);
+insert into ENDERECO(logradouro, cep, numero, cidade, principal) values ('Rua Arlindo Nogueira', '64000290', '10', 'Teresina', false);
+insert into ENDERECO(logradouro, cep, numero, cidade, principal) values ('Avenida Rio Branco', '20040002', '35', 'Rio de Janeiro', false);
+insert into ENDERECO(logradouro, cep, numero, cidade, principal) values ('Rua Domingos Olímpio', '62011140', '425', 'Sobral', false);
+insert into ENDERECO(logradouro, cep, numero, cidade, principal) values ('Avenida Governador José Malcher', '66055260', '36', 'Belém', false);
+insert into ENDERECO(logradouro, cep, numero, cidade, principal) values ('Rua da Imprensa', '79002290', '641', 'Campo Grande', false);
+insert into ENDERECO(logradouro, cep, numero, cidade, principal) values ('Rua Serra de Bragança', '03318000', '23', 'São Paulo', false);
+insert into ENDERECO(logradouro, cep, numero, cidade, principal) values ('Travessa Antônio Ferreira', '68700216', '568', 'Capanema', false);
+insert into ENDERECO(logradouro, cep, numero, cidade, principal) values ('Rua dos Carijós', '30120060', '130', 'Belo Horizonte', false);
+insert into ENDERECO(logradouro, cep, numero, cidade, principal) values ('Rodovia Raposo Tavares', '06709015', '95', 'Cotia', false);
