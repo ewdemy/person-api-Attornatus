e0d8bb0197a5b5201391e8a4562cae5ce5e2a97a
ewdemy <ewdemy@hotmail.com>
2023-01-13 00:06:34 -0300

    Controller endereço

--- src/main/java/com/mrcruz/personapi/controller/EnderecoController.java
+++ src/main/java/com/mrcruz/personapi/controller/EnderecoController.java
@@ -1,9 +1,44 @@
 package com.mrcruz.personapi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mrcruz.personapi.model.Endereco;
+import com.mrcruz.personapi.service.EnderecoService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/person-api/enderecos")
 public class EnderecoController {
+
+    private EnderecoService enderecoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Endereco salvar(@RequestBody Endereco endereco){
+        return enderecoService.salvar(endereco);
+    }
+
+    @GetMapping
+    public Page<Endereco> listar(Pageable pageable){
+        return enderecoService.listar(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public Endereco buscar(@PathVariable Long id){
+        return enderecoService.buscar(id);
+    }
+
+    @PutMapping("{id}")
+    public Endereco atualizar(@PathVariable Long id, @RequestBody Endereco endereco){
+        return enderecoService.atualizar(id, endereco);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Long id){
+        enderecoService.deletar(id);
+    }
 }
