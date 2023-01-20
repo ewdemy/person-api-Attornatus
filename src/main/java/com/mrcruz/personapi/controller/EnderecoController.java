cc9c628d2bd761b045c670b9f452754aff7b2ab8
ewdemy <ewdemy@hotmail.com>
2023-01-20 02:33:02 -0300

    Projeto finalizado

--- src/main/java/com/mrcruz/personapi/controller/EnderecoController.java
+++ src/main/java/com/mrcruz/personapi/controller/EnderecoController.java
@@ -2,6 +2,8 @@ package com.mrcruz.personapi.controller;
 
 import com.mrcruz.personapi.model.Endereco;
 import com.mrcruz.personapi.service.EnderecoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
@@ -10,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Endereço Controller")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/enderecos")
@@ -17,27 +20,32 @@ public class EnderecoController {
 
     private EnderecoService enderecoService;
 
+    @Operation(summary = "Salvar Novo Endereço")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Endereco salvar(@RequestBody @Valid Endereco endereco){
         return enderecoService.salvar(endereco);
     }
 
+    @Operation(summary = "Listar Endereços")
     @GetMapping
     public Page<Endereco> listar(@ParameterObject Pageable pageable){
         return enderecoService.listar(pageable);
     }
 
+    @Operation(summary = "Buscar Endereço por Id")
     @GetMapping("/{id}")
     public Endereco buscar(@PathVariable Long id){
         return enderecoService.buscar(id);
     }
 
+    @Operation(summary = "Atualizar Endereço")
     @PutMapping("{id}")
     public Endereco atualizar(@PathVariable Long id, @RequestBody @Valid Endereco endereco){
         return enderecoService.atualizar(id, endereco);
     }
 
+    @Operation(summary = "Deletar Endereço")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar(@PathVariable Long id){
--- src/main/java/com/mrcruz/personapi/controller/PessoaController.java
+++ src/main/java/com/mrcruz/personapi/controller/PessoaController.java
@@ -5,6 +5,8 @@ import com.mrcruz.personapi.model.Pessoa;
 import com.mrcruz.personapi.model.PessoaDTO;
 import com.mrcruz.personapi.model.PessoaRequest;
 import com.mrcruz.personapi.service.PessoaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
+@Tag(name = "Pessoa Controller")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/pessoas")
@@ -22,48 +25,57 @@ public class PessoaController {
 
     private PessoaService pessoaService;
 
+    @Operation(summary = "Salvar Nova Pessoa")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Pessoa salvar(@RequestBody @Valid PessoaRequest pessoa){
         return pessoaService.salvar(pessoa);
     }
 
+    @Operation(summary = "Listar Pessoas")
     @GetMapping
     public Page<PessoaDTO> listar(@RequestParam(required = false) String nome, @ParameterObject Pageable pageable){
         return pessoaService.listar(nome, pageable);
     }
 
+    @Operation(summary = "Buscar Pessoa por Id")
     @GetMapping("/{id}")
     public Pessoa buscar(@PathVariable Long id){
         return pessoaService.buscar(id);
     }
 
+    @Operation(summary = "Atualizar Pessoa")
     @PutMapping("/{id}")
     public Pessoa atualizar(@PathVariable Long id, @RequestBody @Valid PessoaRequest pessoa){
         return pessoaService.atualizar(id, pessoa);
     }
 
+    @Operation(summary = "Deletar Pessoa")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar(@PathVariable Long id){
         pessoaService.deletar(id);
     }
 
+    @Operation(summary = "Adicionar Novo Endereço para Pessoa")
     @PutMapping("/{idPessoa}/adicionar-endereco/{idEndereco}")
     public Pessoa adicionarEndereco(@PathVariable Long idPessoa, @PathVariable Long idEndereco){
         return pessoaService.adicionarEndereco(idPessoa, idEndereco);
     }
 
+    @Operation(summary = "Adicionar Endereço Principal para Pessoa")
     @PutMapping("/{idPessoa}/adicionar-endereco-principal/{idEndereco}")
     public Pessoa adicionarEnderecoPrincipal(@PathVariable Long idPessoa, @PathVariable Long idEndereco){
         return pessoaService.adicionarEnderecoPrincipal(idPessoa, idEndereco);
     }
 
+    @Operation(summary = "Remover Endereço da Pessoa")
     @PutMapping("/{idPessoa}/remover-endereco/{idEndereco}")
     public Pessoa removerEndereco(@PathVariable Long idPessoa, @PathVariable Long idEndereco){
         return pessoaService.removerEndereco(idPessoa, idEndereco);
     }
 
+    @Operation(summary = "Listar Endereços da Pessoa")
     @GetMapping("/{idPessoa}/enderecos")
     public Set<Endereco> buscarEnderecosPessoa(@PathVariable Long idPessoa){
         return pessoaService.buscarEnderecosPessoa(idPessoa);
